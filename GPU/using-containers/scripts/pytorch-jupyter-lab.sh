e8246f24ddc9cdb9deb955b5ff8f2223d16172b9
Peter Molnar <7682728+kingmolnar@users.noreply.github.com>
2024-06-05 18:12:22 -0400

    Update pytorch-jupyter-lab.sh

--- GPU/using-containers/scripts/pytorch-jupyter-lab.sh
+++ GPU/using-containers/scripts/pytorch-jupyter-lab.sh
@@ -31,7 +31,7 @@ podman system migrate
 
 podman run \
 	-p ${FREE_PORT}:8888 \
-	--device nvidia.com/gpu=all \
+	--device nvidia.com/gpu=0 \
 	--security-opt=label=disable \
 	-v $PWD:/myhome -v /data:/mydata \
 	 nvcr.io/nvidia/pytorch:24.01-py3 jupyter-lab
